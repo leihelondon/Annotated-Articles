620df985139d6f1fa9f3bf665dcefe9f6601a13a
Lei He <leihe023@gmail.com>
2015-09-28 12:19:48 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/'));
+
+// views is directory for all template files
+app.set('css', __dirname + '/css');
+app.set('external', __dirname + '/external');
+app.set('js', __dirname + '/js');
+app.set('node_modules', __dirname + '/node_modules');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
